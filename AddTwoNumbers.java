99462b95152dd934258c2f9e992c81c45291fab7
bijalbenny <145442911+bijalbenny@users.noreply.github.com>
2023-09-19 16:11:55 +0530

    Addition of Two Numbers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,21 @@
+/***************************************************************************
+ * File          :          AddTwoNumbers.java
+ * Descriptiopn  :          Addition of Two Numbers using Java
+ * Author        :          Bijal T Benny
+ * Version       :          1.0
+ * Date          :          18/09/2023
+ * *************************************************************************/
+public class AddTwoNumbers {
+	public static void main (String[] args) {
+		int number1=10,number2=7,difference,multiplication;
+		float division;
+        int sum=number1+number2;
+        difference=number1-number2;
+        multiplication=number1*number2;
+        division=(float)number1/number2;
+        System.out.println("Sum="+sum);
+        System.out.println("Difference="+difference);
+        System.out.println("Multiplication="+multiplication);
+        System.out.println("Division="+division);
+}
+}
