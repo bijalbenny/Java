203d1007b070398ead84240acae5880179d62cf0
bijalbenny <145442911+bijalbenny@users.noreply.github.com>
2023-09-19 16:12:37 +0530

    Difference of two numbers

---
+++ DiferenceOfTwoNumbers.java
@@ -0,0 +1,10 @@
+
+public class DiferenceOfTwoNumbers {
+	public static void main (String[] args) {
+
+	int number1=10,number2=7;
+	int difference=number1-number2;
+	System.out.println("Difference="+difference);}
+
+
+}
