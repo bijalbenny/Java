10f7d4aa089c3dbb64e3161975e7e29b8575e387
bijalbenny <145442911+bijalbenny@users.noreply.github.com>
2023-09-19 16:13:50 +0530

    Multiplication of Two Numbers

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+
+public class Multiplication {
+	public static void main (String[] args) {
+
+		int number1=10,number2=7;
+		int product=number1*number2;
+		System.out.println("Product="+product);}
+
+
+	}
+
+
