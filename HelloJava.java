85ff542b2fe893be623af6d0c065a69251e05e36
bijalbenny <145442911+bijalbenny@users.noreply.github.com>
2023-09-19 15:03:27 +0530

    My first java file

---
+++ HelloJava.java
@@ -0,0 +1,14 @@
+/***************************************************************************
+ * File          :          HelloJava.java
+ * Descriptiopn  :          My first Java program the "HelloJava"
+ * Author        :          Bijal T Benny
+ * Version       :          1.0
+ * Date          :          18/09/2023
+ * *************************************************************************/
+public class HelloJava {
+	public static void main (String[] args) {
+		System.out.print("Hello Java");
+		System.out.println(" Hai Bijal");
+	}
+
+}
