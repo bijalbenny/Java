5efdd04d7a88de2ecbce80ed02d41ec302f8713e
bijalbenny <145442911+bijalbenny@users.noreply.github.com>
2023-09-19 16:13:10 +0530

    Division of two numbers

---
+++ Division.java
@@ -0,0 +1,11 @@
+
+public class Division {
+	public static void main (String[] args) {
+
+		int number1=10,number2=7;
+		float division=(float)number1/number2;
+		System.out.println("division="+division);}
+
+
+	}
+
